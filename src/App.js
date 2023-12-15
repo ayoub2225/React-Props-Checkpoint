ec760e9086641a753a08762e41f1c40a9bd7005a
ayoub2225 <hamouda27bensalah@gmail.com>
2023-12-15 11:28:29 +0100

    jsx checkpoint

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nav from "./Nav";
+import PlayerList from "./PlayerList";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <PlayerList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <div className="nav">
+        <img
+          className="img"
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1200px-Arsenal_FC.svg.png"
+          alt=""
+        />
+        <div className="ea">Arsenal Team 2k24</div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
